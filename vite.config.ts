afd7b9a3eea8e0a9efe342c02aa86a5a6230ef89
IgorBaio <37658514+IgorBaio@users.noreply.github.com>
2025-06-26 11:09:23 -0300

    config the workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,30 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      manifest: {
+        name: 'Catalogo Admin',
+        short_name: 'Admin',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+      },
     }),
+    tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    host: true,
+    allowedHosts: ['.baiosystems.com.br', 'igorbaio.github.io']
+  },
+  base: "/catalogo-virtual-admin/"
 })
